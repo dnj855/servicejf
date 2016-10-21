f9b113f9d27c6e1abf1d63103c4b07642639187e
dnj855 <dnj855@yahoo.com>
2016-10-21 18:53:53 +0200

    Redesign de la barre de navigation et intégration des plugins javascript

--- index.php
+++ index.php
@@ -7,69 +7,72 @@ include('auth.php');
     <head>
         <meta charset="utf-8" />
         <title>Bienvenue sur le portail du service j&f:</title>
-        <link href="design.css" rel="stylesheet" type="text/css" />
-        <link href="auth.css" rel="stylesheet" type="text/css" />
+        <link href="assets/css/bootstrap.min.css" rel="stylesheet" type="text/css" />
+        <link href="assets/css/bootstrap-theme.min.css" rel="stylesheet" type="text/css" />
     </head>
 
     <body>
-<?php include('auth_menu_utilisateur.php') ?>
-<?php include('nav_menu.php') ?>
-<div class="index">
-	<p align="center"><img src="logo.png" alt="Logo" width="500px" class="logo"></p>
-	<p class="index_titre">voici les jeux en cours :</p>
-	<table class="index">
-		<tr>
-			<td class="index_td_left">
-				<p class="index_titre_td">Le challenge de l'invité</p>
-			</td>
-			<td class="index_td_right">
-				<p class="index_titre_td">Le challenge des soirées foot</p>
-			</td>
-		</tr>
-		<tr>	
-			<td class="index_td_left">
-				<p class="index_description_td">
-				Direct ou PAD ? Studio ou téléphone ?<br />Le challenge de l'invité est le point fondateur du service j&f:
-				</p>
-				<p class="index_liens">
-				<?php if($_SESSION['service'] == '1') { ?>
-				<a href="ci.php">Page à remplir pour l'intervieweur</a><br />
-				<?php } ?>
-				<a href="ci_resultats.php">Consultation des données</a>
-				</p>
-			</td>
-			<td class="index_td_right">
-				<p class="index_description_td">
-				Sur proposition du président d'honneur, voyons quel technicien porte le plus chance au FC Metz.
-				</p>
-				<p class="index_liens">
-				<?php if($_SESSION['css'] == '1') { ?>
-				<a href="css.php">La page à remplir après chaque journée de championnat</a><br />
-				<?php } ?>
-				<a href="css_resultats.php">Consultation des données</a>
-				</p>
-			</td>
-		</tr>
-		<tr>
-			<td class="index_td_left">
-				<p class="index_titre_td">Une boite à idées</p>
-			</td>
-			<td class="index_td_right">
-			</td>
-		</tr>
-		<tr>
-			<td class="index_td_left">	
-				<p class="index_description_td">
-				Toujours à l'écoute, le service j&f: vous propose de nous donner vos meilleures idées de jeux et de festivités.
-				</p>
-				<p class="index_liens">
-				<a href="bai.php">Cliquez ici, tout simplement</a>
-			</td>
-			<td class="index_td_right">
-			</td>
-		</tr>
-	</table>
-</div>
-
-</body>
+        <?php include('nav_menu.php') ?>
+        <div class="container">
+            <?php include('auth_menu_utilisateur.php') ?>
+            <p align="center"><img src="logo.png" alt="Logo" width="500px" class="logo"></p>
+            <p class="index_titre">voici les jeux en cours :</p>
+            <table class="index">
+                <tr>
+                    <td class="index_td_left">
+                        <p class="index_titre_td">Le challenge de l'invité</p>
+                    </td>
+                    <td class="index_td_right">
+                        <p class="index_titre_td">Le challenge des soirées foot</p>
+                    </td>
+                </tr>
+                <tr>
+                    <td class="index_td_left">
+                        <p class="index_description_td">
+                            Direct ou PAD ? Studio ou téléphone ?<br />Le challenge de l'invité est le point fondateur du service j&f:
+                        </p>
+                        <p class="index_liens">
+                            <?php if ($_SESSION['service'] == '1') { ?>
+                                <a href="ci.php">Page à remplir pour l'intervieweur</a><br />
+                            <?php } ?>
+                            <a href="ci_resultats.php">Consultation des données</a>
+                        </p>
+                    </td>
+                    <td class="index_td_right">
+                        <p class="index_description_td">
+                            Sur proposition du président d'honneur, voyons quel technicien porte le plus chance au FC Metz.
+                        </p>
+                        <p class="index_liens">
+                            <?php if ($_SESSION['css'] == '1') { ?>
+                                <a href="css.php">La page à remplir après chaque journée de championnat</a><br />
+                            <?php } ?>
+                            <a href="css_resultats.php">Consultation des données</a>
+                        </p>
+                    </td>
+                </tr>
+                <tr>
+                    <td class="index_td_left">
+                        <p class="index_titre_td">Une boite à idées</p>
+                    </td>
+                    <td class="index_td_right">
+                    </td>
+                </tr>
+                <tr>
+                    <td class="index_td_left">
+                        <p class="index_description_td">
+                            Toujours à l'écoute, le service j&f: vous propose de nous donner vos meilleures idées de jeux et de festivités.
+                        </p>
+                        <p class="index_liens">
+                            <a href="bai.php">Cliquez ici, tout simplement</a>
+                    </td>
+                    <td class="index_td_right">
+                    </td>
+                </tr>
+            </table>
+        </div>
+        <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
+        <!-- Include all compiled plugins (below), or include individual files as needed -->
+        <script src="assets/js/bootstrap.min.js"></script>
+    </body>
 </html>
--- nav_menu.php
+++ nav_menu.php
@@ -1,23 +1,30 @@
-<div class="nav">
-<table class="nav_table">
-<tr>
-	<td>
-		<img class="logo_small" src="logosmall.png" alt="logosmall"/>
-	</td>
-	<td class="nav_td">
-		<a href="index.php">Retour à l'accueil</a> - 
-		<em class="nav_strong">Challenge invité : </em>
-		<?php if($_SESSION['service'] == '1') { ?>
-			<a href="ci.php">saisir un invité</a> / 
-		<?php } ?>
-		<a href="ci_resultats.php">consulter les résultats</a> - 
-		<em class="nav_strong">Challenge des soirées foot : </em> 
-		<?php if($_SESSION['css'] == '1') { ?>
-			<a href="css.php">saisir une soirée foot</a> / 
-		<?php } ?>
-		<a href="css_resultats.php">consulter les résultats</a> - 			
-	<a href="bai.php">Boite à idées</a>
-	</td>
-</tr>
-</table>
-</div>
+<nav class="navbar navbar-default navbar-fixed-top">
+    <div class="container-fluid">
+        <div class="navbar-header">
+            <a href='index.php' class="navbar-brand"><img height="27px" src="logosmall.png" alt="logosmall"/></a>
+        </div>
+        <div class="collapse navbar-collapse">
+            <ul class="nav navbar-nav">
+                <li class="dropdown">
+                    <a data-target="#" class="dropdown-toggle" data-toggle="dropdown">Challenge invité <span class="caret"></span></a>
+                    <ul class="dropdown-menu">
+                        <?php if ($_SESSION['service'] == '1') { ?>
+                            <li><a href="ci.php">Saisir un invité</a></li>
+                        <?php } ?>
+                        <li><a href="ci_resultats.php">Consulter les résultats</a></li>
+                    </ul>
+                </li>
+                <li class="dropdown">
+                    <a data-target="#" class="dropdown-toggle" data-toggle="dropdown">Challenge des soirées sport <span class="caret"></span></a>
+                    <ul class="dropdown-menu">
+                        <?php if ($_SESSION['css'] == '1') { ?>
+                            <li><a href="css.php">Saisir une soirée foot</a></li>
+                        <?php } ?>
+                        <li><a href="css_resultats.php">Consulter les résultats</a></li>
+                    </ul>
+                </li>
+                <li> <a href="bai.php">Boite à idées</a></li>
+            </ul>
+        </div>
+    </div>
+</nav>
