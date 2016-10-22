6698296b78954cb5a1229b11547f68d2bd35e122
dnj855 <dnj855@yahoo.com>
2016-10-22 11:52:25 +0200

    Correction d'une requête mySql pour permettre à la variable $mess_nonlus d'être NULL.

--- auth.php
+++ auth.php
@@ -1,9 +1,10 @@
 <?php
+
 session_start();
 include('auth_inc_connectdb.php');
 
 // On va récupérer le nombre de messages non lus.
-$query = $bdd->prepare('SELECT COUNT(lu) AS mess_nonlus FROM mess WHERE id_receiver = ? AND lu = 0');
+$query = $bdd->prepare('SELECT COUNT(lu) AS mess_nonlus FROM mess WHERE id_receiver = ? AND lu = 0 GROUP BY id_receiver');
 $query->execute(array($_SESSION['id']));
 $mess_nonlus = $query->fetch();
 $mess_nonlus = $mess_nonlus['mess_nonlus'];
@@ -13,15 +14,12 @@ $mess_lus = $query->fetch();
 $mess_lus = $mess_lus['mess_lus'];
 
 
-if($_GET['log'] == 'yes') { // On regarde si l'utilisateur vient juste de se logger, auquel cas, on renvoie vers l'index.
-	header('location:index.php');
-}
-elseif(isset($_SESSION['id'])) { // Dans tous les autres cas de figure, on regarde d'abord si l'utilisateur est déjà loggé. Auquel cas, on continue le chargement de la page.
-}
-elseif(isset($_GET['log'])) {
-	include('auth_formulaire.php');
-}
-else {
-	header('location:auth.php?log=new');
+if ($_GET['log'] == 'yes') { // On regarde si l'utilisateur vient juste de se logger, auquel cas, on renvoie vers l'index.
+    header('location:index.php');
+} elseif (isset($_SESSION['id'])) { // Dans tous les autres cas de figure, on regarde d'abord si l'utilisateur est déjà loggé. Auquel cas, on continue le chargement de la page.
+} elseif (isset($_GET['log'])) {
+    include('auth_formulaire.php');
+} else {
+    header('location:auth.php?log=new');
 }
 ?>
