cf0c678e1bb02f63d4435d7012985e7a15f0f8da
dnj855 <dnj855@yahoo.com>
2016-10-22 17:59:37 +0200

    - refonte du design des pages départ et retour de personnel.

--- ar_depart_personnel.php
+++ ar_depart_personnel.php
@@ -1,40 +1,76 @@
 <?php
 include('auth.php');
-if($_SESSION['admin'] == 0) {
+if ($_SESSION['admin'] == 0) {
     header('location:index.php');
 }
 
-        $_GET['id'] = (int) $_GET['id']; //Vérification de l'id reçue.
+$_GET['id'] = (int) $_GET['id']; //Vérification de l'id reçue.
 
-        $query = $bdd->prepare('SELECT * FROM personnel_fbln WHERE id = ?');
-        $query->execute(array($_GET['id']));
+$query = $bdd->prepare('SELECT * FROM personnel_fbln WHERE id = ?');
+$query->execute(array($_GET['id']));
 
-        $personnel=$query->fetch();
+$personnel = $query->fetch();
+?>
 
-        ?>
-
-            <!DOCTYPE html>
-    <html>
+<!DOCTYPE html>
+<html>
     <head>
         <meta charset="utf-8" />
-        <title>Signaler le départ de <?php echo $personnel['prenom'] . ' ' . $personnel['nom'];?></title>
-        <link href="design.css" rel="stylesheet" type="text/css" />
+        <title>Signaler le départ d'un participant</title>
+        <link href="assets/css/bootstrap.min.css" rel="stylesheet" type="text/css" />
+        <link href="assets/css/bootstrap-theme.min.css" rel="stylesheet" type="text/css" />
+        <link href="assets/css/design.css" rel="stylesheet" type="text/css" />
     </head>
 
     <body>
-    <?php include('nav_menu.php');
-    include('auth_menu_utilisateur.php');
-    ?>
-    <div class="index">
-    <p class="titre">Départ de <?php echo $personnel['prenom'] . ' ' . $personnel['nom'] ?></p>
-    <div class="formulaire">
-    <table class="formulaire">
-    <form method="post" action="ar_depart_personnel_traitement.php">
-    <tr><td><input type="checkbox" name="depart" class="formulaire"></td><td>Cocher la case pour confirmer.</td></tr>
-    <tr><td></td><td>
-    <input type="hidden" name="id" value="<?echo $personnel['id'] ?>">
-    <input type="submit" class="envoi_formulaire"></td></tr>
-    </form></table></div>
-    </div>
+        <?php
+        include('nav_menu.php');
+        ?>
+        <div class="container">
+            <header>
+                <h1>départ d'un participant</h1>
+            </header>
+            <section class="row">
+                <!--Navigation spécifique pour l'espace de gestion du personnel. Penser à changer la classe "active" !-->
+                <nav class="col-md-4">
+                    <div class="panel panel-primary">
+                        <div class="panel-body">
+                            <ul class="nav-pills nav nav-stacked">
+                                <li><a href="ar_modifier_personnel.php?id=<?php echo $_GET['id']; ?>"><span class="glyphicon glyphicon-cog"></span> Modifier les informations générales</a></li>
+                                <li><a href="ar_modif_mdp_personnel.php?id=<?php echo $_GET['id']; ?>"><span class="glyphicon glyphicon-lock"></span> Modifier le mot de passe</a></li>
+                                <li class="active"><a href="ar_depart_personnel.php?id=<?php echo $_GET['id']; ?>"><span class="glyphicon glyphicon-remove"></span> Faire partir le participant</a></li>
+                                <li><a href="ar_affichage_personnel.php"><span class="glyphicon glyphicon-chevron-left"></span> Retour à la liste générale</a></li>
+                            </ul>
+                        </div>
+                    </div>
+                </nav>
+                <div class="col-md-8">
+                    <div class="panel panel-primary">
+                        <div class="panel-heading">
+                            <?php echo $personnel['prenom'] . ' ' . $personnel['nom']; ?>
+                        </div>
+                        <div class="panel-body">
+                            <form method="post" action="ar_depart_personnel_traitement.php">
+                                <fieldset>
+                                    <div class="form-group">
+                                        <div class="checkbox">
+                                            <label><input type="checkbox" name="depart">Cocher la case pour confirmer.</label>
+                                        </div>
+                                    </div>
+                                    <div class="form-group">
+                                        <input type="hidden" name="id" value="<?php echo $personnel['id']; ?>">
+                                        <button type="submit" class="btn btn-primary pull-right"><span class="glyphicon glyphicon-ok"></span> Valider</button>
+                                    </div>
+                                </fieldset>
+                            </form>
+                        </div>
+                    </div>
+                </div>
+            </section>
+        </div>
+        <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
+        <!-- Include all compiled plugins (below), or include individual files as needed -->
+        <script src="assets/js/bootstrap.min.js"></script>
     </body>
-    </html>
+</html>
--- ar_retour_personnel.php
+++ ar_retour_personnel.php
@@ -1,6 +1,6 @@
 <?php
 include('auth.php');
-if($_SESSION['admin'] == 0) {
+if ($_SESSION['admin'] == 0) {
     header('location:index.php');
 }
 ?>
@@ -8,26 +8,47 @@ if($_SESSION['admin'] == 0) {
 <html>
     <head>
         <meta charset="utf-8" />
-        <title>Les participants partis</title>
-        <link href="design.css" rel="stylesheet" type="text/css" />
+        <title>Faire revenir un participant</title>
+        <link href="assets/css/bootstrap.min.css" rel="stylesheet" type="text/css" />
+        <link href="assets/css/bootstrap-theme.min.css" rel="stylesheet" type="text/css" />
+        <link href="assets/css/design.css" rel="stylesheet" type="text/css" />
     </head>
 
     <body>
-    <?php include('nav_menu.php');
-    include('auth_menu_utilisateur.php');
-    ?>
-    <div class="index">
-    <p class="titre">Faire revenir un participant</p>
-    <div class="ar_personnel">
-    <table class="ar_personnel">
-    <?php
-    $participants_partis = $bdd->query('SELECT id, nom, prenom FROM personnel_fbln WHERE actif = 0');
-    while($pp=$participants_partis->fetch()){
-        echo '<tr><td>' . $pp['prenom'] . ' ' . $pp['nom'] . '</td><td><a href="ar_retour_personnel_traitement.php?id=' . $pp['id'] . '" class="ar_personnel">faire revenir</a></td></tr>';
-    }
-?>
-</table>
-</div>
-</div>
-</body>
+        <?php
+        include('nav_menu.php');
+        ?>
+        <div class="container">
+            <header>
+                <h1>faire revenir un participant</h1>
+            </header>
+
+            <section class="row">
+                <div class="col-md-6 col-md-offset-3">
+                    <div class="panel panel-primary">
+                        <div class="panel-heading">
+                            Les participants partis
+                        </div>
+                        <ul class="list-group">
+                            <?php
+                            $participants_partis = $bdd->query('SELECT id, nom, prenom FROM personnel_fbln WHERE actif = 0');
+                            while ($pp = $participants_partis->fetch()) {
+                                ?>
+
+                                <li class="list-group-item">
+                                    <?php echo $pp['prenom'] . ' ' . $pp['nom']; ?> <a href="ar_retour_personnel_traitement.php?id=<?php echo $pp['id']; ?>"><span class="glyphicon glyphicon-repeat pull-right"></span></a>
+                                </li>
+                                <?php
+                            }
+                            ?>
+                        </ul>
+                    </div>
+                </div>
+            </section>
+        </div>
+        <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
+        <!-- Include all compiled plugins (below), or include individual files as needed -->
+        <script src="assets/js/bootstrap.min.js"></script>
+    </body>
 </html>
